4396a2093cb5c56ec6435e23007207779698e18a
abhaylivenow <56759264+abhaylivenow@users.noreply.github.com>
2021-07-13 00:11:07 +0530

    Otp feature added

--- app/src/main/java/com/example/chatapptest/PhoneNumberActivity.java
+++ app/src/main/java/com/example/chatapptest/PhoneNumberActivity.java
@@ -2,13 +2,35 @@ package com.example.chatapptest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.chatapptest.databinding.ActivityPhoneNumberBinding;
 
 public class PhoneNumberActivity extends AppCompatActivity {
+
+    ActivityPhoneNumberBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_phone_number);
+        binding = ActivityPhoneNumberBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().hide();
+
+        binding.phoneBox.requestFocus();
+
+        binding.btnSendOtp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PhoneNumberActivity.this,OTP.class);
+                String phoneNumber = binding.phoneBox.getText().toString();
+                intent.putExtra("phoneNumber", phoneNumber);
+                startActivity(intent);
+            }
+        });
     }
 }
