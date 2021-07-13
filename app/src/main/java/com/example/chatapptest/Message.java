4a2a1886cd448e630e06b71d110893533677462f
abhaylivenow <56759264+abhaylivenow@users.noreply.github.com>
2021-07-14 00:04:40 +0530

    Chat Feature Working

---
+++ app/src/main/java/com/example/chatapptest/Message.java
@@ -0,0 +1,59 @@
+package com.example.chatapptest;
+
+public class Message {
+    private String message;
+    private String messageId;
+    private String senderId;
+    private long timeStamp;
+    private int feelings;
+
+    public Message(){
+        
+    }
+
+    public Message(String message, String senderId, long timeStamp) {
+        this.message = message;
+        this.senderId = senderId;
+        this.timeStamp = timeStamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public int getFeelings() {
+        return feelings;
+    }
+
+    public void setFeelings(int feelings) {
+        this.feelings = feelings;
+    }
+}
